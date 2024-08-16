c08e67fe11eebe8d1725ec0e9af39b752ebef36a
Andrey Plahtiy <aplaxtiy@gmail.com>
2024-08-16 19:32:46 +0300

    draft version

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,41 @@ function App() {
     }
 
     return (
-        <div>
-            <button onClick={playSound}>
-                Play Sound
-            </button>
+        <div className="container">
+            <div className="piano">
+                <div className="header">
+                    <h2>Playable piano</h2>
+                    <div>
+                        <label htmlFor="volume">Volume</label>
+                        <input className="slider" type="range" id="volume" name="volume" min="0" max="100"/>
+                    </div>
+                    <label>
+                        Show keys
+                        <input className="checkbox" type="checkbox" name="piano-keys"/>
+                    </label>
+                </div>
+                <div className="piano-keys">
+                    <div className="white"><span>A</span></div>
+                    <div className="black"><span>W</span></div>
+                    <div className="white"><span>S</span></div>
+                    <div className="black"><span>E</span></div>
+                    <div className="white"><span>D</span></div>
+
+                    <div className="white"><span>F</span></div>
+                    <div className="black"><span>T</span></div>
+                    <div className="white"><span>G</span></div>
+                    <div className="black"><span>Y</span></div>
+                    <div className="white"><span>H</span></div>
+                    <div className="black"><span>U</span></div>
+                    <div className="white"><span>J</span></div>
+
+                    <div className="white"><span>K</span></div>
+                    <div className="black"><span>O</span></div>
+                    <div className="white"><span>L</span></div>
+                    <div className="black"><span>P</span></div>
+                    <div className="white"><span>;</span></div>
+                </div>
+            </div>
         </div>
     )
 }
